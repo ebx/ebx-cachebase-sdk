b4a0eb00c525cef58e0cd3a7bbc3e9f97d06ea68
Daniyar <daniyar.kaliyev@echobox.com>
2021-08-11 17:36:47 +0100

    Release v1.2.1 (#7)
    * TEC-12838
    
    Added periodic cache with callback
    
    * TEC-12838
    
    PR fixes
    
    * TEC-12838
    
    Style fix
    
    * TEC-12838
    
    Switched to lang3 NotImplemented
    
    * TEC-12838
    
    Use Java Function Supplier
    
    * TEC-12838
    
    Removed unused param
    
    * TEC-12838
    
    Renamed class and method to better suit use case
    
    * TEC-11878 Update Travis-CI deployment to v2 (#5)
    
    * TEC-12962  Added CacheWithSupplierFailover implementation (#6)
    
    * TEC-12838
    
    Cosmetic change
    
    * TEC-12838
    
    Added implementation.
    
    * TEC-12838
    
    Added tests
    
    * TEC-12838
    
    Style fixes
    
    * TEC-12838
    
    Trying to fix the tests
    
    * TEC-12838
    
    Trying to run with byte buddy
    
    * TEC-12838
    
    Added javaagent
    
    * TEC-12838
    
    Attempting to fix tests. Use MockUp instead of inline mocks
    
    * TEC-12838
    
    Updated pom
    
    * TEC-12838
    
    PR fixes
    
    * TEC-12962
    
    Updated CHANGELOG.md
    
    * TEC-12962
    
    * TEC-12962
    
    Added test with Memcached.
    PR fixes
    
    * TEC-12962
    
    Updated changelog
    
    * TEC-12962
    
    PR fixes
    
    * TEC-12962
    
    Updated readme
    
    Co-authored-by: João Moreira <joao.moreira@echobox.com>

---
+++ src/main/java/com/echobox/cache/CacheWithSupplierFailover.java
@@ -0,0 +1,114 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.echobox.cache;
+
+import com.google.gson.reflect.TypeToken;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.function.Supplier;
+
+/**
+ * The cache that periodically gets the latest value from the provided source of truth.
+ * If source of truth is not available then it will use
+ * cached value for a specified period of time before throwing an error.
+ * @param <T> return type of cache object
+ * @author Daniyar
+ */
+public class CacheWithSupplierFailover<T extends Serializable> {
+  
+  private static final Logger logger = LoggerFactory.getLogger(CacheWithSupplierFailover.class);
+  
+  private static final String DEFAULT_CACHE_KEY_SUFFIX = "-default";
+  
+  private static final String ERROR_CACHE_KEY_SUFFIX = "-error";
+  
+  private final int defaultCacheSecs;
+  
+  private final int maxCacheSecsOnError;
+  
+  private final CacheService cacheService;
+  
+  private final TypeToken<T> returnType;
+  
+  /**
+   * Constructor
+   *
+   * @param cacheService the cache service
+   * @param returnType return type
+   * @param defaultCacheSecs period in seconds during which we use cache
+   * @param maxCacheSecsOnError The maximum interval we will continue to use the cached value
+   * if the 'source of truth' supplier isn't working for whatever reason.
+   */
+  public CacheWithSupplierFailover(CacheService cacheService, TypeToken<T> returnType,
+      int defaultCacheSecs, int maxCacheSecsOnError) {
+    this.returnType = returnType;
+    this.defaultCacheSecs = defaultCacheSecs;
+    this.maxCacheSecsOnError = maxCacheSecsOnError;
+    this.cacheService = cacheService;
+  }
+  
+  /**
+   * Check if it is within callback period. If it is then return cached data,
+   * otherwise use callback. If it fails and if it is within callback error timeout period
+   * return cached data. On a successful callback result update the cache
+   * @param key cache key
+   * @param sourceOfTruthSupplier the data supplier, e.g API call
+   * @return cached data
+   */
+  public T getWithFailover(String key, Supplier<T> sourceOfTruthSupplier) {
+    // Only use the supplier once every period, otherwise use cached value
+    String defaultKey = key + DEFAULT_CACHE_KEY_SUFFIX;
+    String errorKey = key + ERROR_CACHE_KEY_SUFFIX;
+    T value = cacheService.tryGetCachedItem(defaultKey, returnType);
+    if (Objects.isNull(value)) {
+      try {
+        value = sourceOfTruthSupplier.get();
+        cacheData(defaultKey, value, defaultCacheSecs);
+        cacheData(errorKey, value, maxCacheSecsOnError);
+      } catch (Exception exception) {
+        // Failed to get the value from source of truth. Use cached value
+        String message = String.format("Failed to get the latest value for %s. "
+            + "Trying to get the failover cached value", key);
+        logger.warn(message);
+        value = cacheService.tryGetCachedItem(errorKey, returnType);
+        if (Objects.isNull(value)) {
+          // We exceeded maximum failover interval
+          message = String.format("The maximum failover timeout of %d seconds has expired for"
+                  + " key %s", maxCacheSecsOnError, key);
+          logger.error(message);
+          throw new SourceOfTruthSupplierException(message, exception);
+        }
+      }
+    }
+    return value;
+  }
+  
+  /**
+   * Cache data on a successful callback result
+   * @param key cache key
+   * @param data cache data
+   */
+  private void cacheData(String key, T data, int expiry) {
+    if (cacheService.isCacheAvailable()) {
+      cacheService.trySaveItemToCache(key, expiry, data);
+    }
+  }
+}
